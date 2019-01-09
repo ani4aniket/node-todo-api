e58f73988a7b9c45243b41e12736a1cf00294db7
Aniket Kumar <aniketkumar049@gmail.com>
2019-01-09 23:11:00 +0530

    Test the DELETE /todos/:id route

--- playground/mongoose-remove.js
+++ playground/mongoose-remove.js
@@ -7,6 +7,10 @@ const {User} = require('./../server/models/user');
 // Todo.remove({}).then((result) => {
 //   console.log(result);
 // });
+// 
+// Todo.findOneAndRemove({_id: '5c35e12113a2a9fd0099474f'}).then((todo) => {
+//
+// });
 
 Todo.findByIdAndRemove('5c35e12113a2a9fd0099474f').then((todo) => {
   console.log(todo);
